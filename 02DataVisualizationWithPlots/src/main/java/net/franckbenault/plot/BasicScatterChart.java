ab01adfd79b899dcb11447b768eafb971868ed1e
franck-benault <franck-benault@orange.fr>
2018-08-14 23:21:24 +0200

    add data visualization program

---
+++ 02DataVisualizationWithPlots/src/main/java/net/franckbenault/plot/BasicScatterChart.java
@@ -0,0 +1,21 @@
+package net.franckbenault.plot;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BasicScatterChart extends Application {
+
+	public static void main(String args[]) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		int[] xData = {1, 2, 3, 4, 5};
+		
+
+		stage.show();
+	}
+
+}
