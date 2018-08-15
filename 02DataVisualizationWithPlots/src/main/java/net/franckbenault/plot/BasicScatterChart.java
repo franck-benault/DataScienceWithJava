b735bfb7b87cffca7a3b8a1eabc99aaab4869f97
franck-benault <franck-benault@orange.fr>
2018-08-15 23:39:32 +0200

    complete ScatterChart

--- 02DataVisualizationWithPlots/src/main/java/net/franckbenault/plot/BasicScatterChart.java
+++ 02DataVisualizationWithPlots/src/main/java/net/franckbenault/plot/BasicScatterChart.java
@@ -2,6 +2,11 @@ package net.franckbenault.plot;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.Scene;
 
 public class BasicScatterChart extends Application {
 
@@ -13,8 +18,23 @@ public class BasicScatterChart extends Application {
 	public void start(Stage stage) throws Exception {
 
 		int[] xData = {1, 2, 3, 4, 5};
-		
+		double[] yData = {1.3, 2.1, 3.3, 4.0, 4.8};
 
+		Series<Integer,Double> series = new Series<>();
+		for(int i=0 ; i<xData.length; i++) {
+			series.getData().add(new Data(xData[i], yData[i]));
+		}
+		NumberAxis xAxis = new NumberAxis();
+		xAxis.setLabel("x");
+		NumberAxis yAxis = new NumberAxis();
+		yAxis.setLabel("y");
+		
+		ScatterChart<Integer,Double> scatterChart = new ScatterChart(xAxis,yAxis);
+		scatterChart.getData().add(series);
+		
+		Scene scene = new Scene(scatterChart, 800, 600);
+		stage.setScene(scene);
+		
 		stage.show();
 	}
 
