71894ccd64407305c7685542958918c4c09bf74b
franck-benault <franck-benault@orange.fr>
2018-08-19 23:21:08 +0200

    add linear algebra example Vector

---
+++ 03LinearAlgebra/src/main/java/net/franckbenault/linearalgebra/RealVectorExample.java
@@ -0,0 +1,32 @@
+package net.franckbenault.linearalgebra;
+
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVector;
+
+public class RealVectorExample {
+
+	public static void main(String[] args) {
+		
+		//constructor with size
+		int size = 3;
+		RealVector vector = new ArrayRealVector(size);
+		System.out.println(vector);
+		
+		//constructor with data
+		double[] data = {1.0, 6.0, 2.5, 2.3};
+		vector = new ArrayRealVector(data);
+		System.out.println(vector);
+		
+		//constructor with size and default value
+		size = 4;
+		double defaultValue = 5.3;
+		vector = new ArrayRealVector(size, defaultValue);
+		System.out.println(vector);
+		
+		//constructor with another vector
+		RealVector vector2 = new ArrayRealVector(vector);
+		System.out.println(vector2);
+
+	}
+
+}
