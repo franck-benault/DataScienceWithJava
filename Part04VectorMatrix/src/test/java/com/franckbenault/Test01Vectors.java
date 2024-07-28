4b78bb3a27e51db3f8925e92a9b72180b28ea035
LAPTOP-UIPBLUVT\franc <franck-benault@orange.fr>
2024-07-28 23:09:02 +0200

    [MATRIX] add example spare matrix and vector

--- Part04VectorMatrix/src/test/java/com/franckbenault/Test01Vectors.java
+++ Part04VectorMatrix/src/test/java/com/franckbenault/Test01Vectors.java
@@ -1,24 +1,70 @@
 package com.franckbenault;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.OpenMapRealVector;
 import org.apache.commons.math3.linear.RealVector;
 import org.junit.jupiter.api.Test;
 
 public class Test01Vectors {
 
 	@Test
-	public void FirstVector01Test() {
+	public void createVector01Test() {
 		
-		RealVector vector1 = new ArrayRealVector(4);
+		//Create an empty vector with a defined size
+		int size = 4;
+		RealVector vector1 = new ArrayRealVector(size);
 		System.out.println(vector1);
 		
+		//Create a vector from a tab
 		double[] data = {1.8, 9.0, 3.14};
 		RealVector vector2 = new ArrayRealVector(data);
 		System.out.println(vector2);
 		
+		//Create a vector from ... a vector
 		RealVector vector3 = new ArrayRealVector(vector2);
 		System.out.println(vector3);
+		
+		//Create a vector defining the size and the default value
+		size = 3;
+		double defaultValue = 3.14;
+		RealVector vector4 = new ArrayRealVector(size,defaultValue);
+		System.out.println(vector4);
+	}
+	
+	@Test
+	public void spareVector01Test() {
+		//spare vector = large vector with a lot of 0
+		RealVector vector1 = new ArrayRealVector(71_000_000);
+		RealVector vector2 = vector1.copy();
+		vector2.mapMultiply(2);
+		
+		assertEquals(vector1, vector2);
+		
+	}
+	
+	@Test
+	public void spareVector02Test() {
+		//spare vector = large vector with a lot of 0
+		RealVector vector1 = new OpenMapRealVector(71_000_000);
+		RealVector vector2 = vector1.copy();
+		vector2.mapMultiply(2);
+		
+		assertEquals(vector1, vector2);
+		
+	}
+	
+	@Test
+	public void spareVector03Test() {
+		//spare vector = large vector with a lot of 0
+		RealVector vector1 = new ArrayRealVector(71_000_000);
+		RealVector vector2 = vector1.copy();
+		vector2.mapMultiply(2);
+		
+		assertEquals(vector1, vector2);
+		
 	}
 
 }
--- Part04VectorMatrix/src/test/java/com/franckbenault/Test02Matrix.java
+++ Part04VectorMatrix/src/test/java/com/franckbenault/Test02Matrix.java
@@ -1,16 +1,72 @@
 package com.franckbenault;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.OpenMapRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.junit.jupiter.api.Test;
 
 public class Test02Matrix {
 
 	@Test
-	public void FirstMatrix01Test() {
+	public void firstMatrix01Test() {
 		
-		RealMatrix matrix1 = new Array2DRowRealMatrix(3,4);
+		//Create an empty matrix defining its size
+		int rowDimension =3;
+		int colDimension =4;
+		RealMatrix matrix1 = new Array2DRowRealMatrix(rowDimension, colDimension);
 		System.out.println(matrix1);
+		
+		//Create a matrix from a java tab
+		double [][] data = {
+				{1.3, 1.5, 1.7, 1.8},
+				{2.3, 1.5, 1.7, 2.9},
+				{3.3, 1.5, 1.7, 3.9}
+		};
+		
+		RealMatrix matrix2 = new Array2DRowRealMatrix(data);
+		System.out.println(matrix2);
+		
+		//create a matrix from another matrix
+		RealMatrix matrix3 = matrix2.copy();
+		System.out.println(matrix3);
+	}
+	
+	@Test
+	public void spareMatrix01Test() {
+		
+		//Spare matrix = large matrix with a lot of 0
+		//In French language matrice creuse
+		RealMatrix spareMatrix = new Array2DRowRealMatrix(10_000, 10_000);
+		
+		RealMatrix spareMatrix2 = spareMatrix.transpose();
+		assertEquals(spareMatrix, spareMatrix2);
+		
+
+	}
+	
+	@Test
+	public void spareMatrix02Test() {
+		
+		//Spare matrix = large matrix with a lot of 0
+		//In French language matrice creuse
+		RealMatrix spareMatrix = new OpenMapRealMatrix(10_000, 10_000);
+		
+		RealMatrix spareMatrix2 = spareMatrix.transpose();
+		assertEquals(spareMatrix, spareMatrix2);
+		
+	}
+	
+	@Test
+	public void spareMatrix03Test() {
+		
+		//Spare matrix = large matrix with a lot of 0
+		//In French language matrice creuse
+		RealMatrix spareMatrix = new Array2DRowRealMatrix(10_000, 10_000);
+		
+		RealMatrix spareMatrix2 = spareMatrix.transpose();
+		assertEquals(spareMatrix, spareMatrix2);
 		
 
 	}
